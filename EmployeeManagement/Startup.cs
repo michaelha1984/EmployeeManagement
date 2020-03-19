07f26fbd8e73a2a8450a57a5a1bfb6c4a35b8b61
Michael Ha <michael.ha@ignitionadvice.com>
2020-03-19 16:05:22 -0700

    Update start up with mvc service and middeware to use default home route.

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -25,6 +25,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,10 +37,11 @@ namespace EmployeeManagement
             }
                         
             app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async context =>
             {
-                await context.Response.WriteAsync("Hosting Env: " + env.EnvironmentName);
+                await context.Response.WriteAsync("Hello World");
             });
         }
     }
