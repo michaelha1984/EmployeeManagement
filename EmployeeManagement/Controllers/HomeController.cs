9d7778c1a69b6d878b363bf1d3370dad0966ebd5
Michael Ha <michael.ha@ignitionadvice.com>
2020-03-19 17:52:23 -0700

    Update controller to return details as a ViewBag in cshtml.

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -23,8 +23,8 @@ namespace EmployeeManagement.Controllers
         public ViewResult Details()
         {
             var model = employeeRepository.GetEmployee(1);
-            ViewData["Employee"] = model;
-            ViewData["PageTitle"] = "Employee Details";
+            ViewBag.Employee = model;
+            ViewBag.PageTitle = "Employee Details";
             return View();
         }
     }
