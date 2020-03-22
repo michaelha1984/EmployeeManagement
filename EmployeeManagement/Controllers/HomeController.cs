1b342462b3b3b4018cde2c885337f9cf67eef6c8
Michael Ha <michael.ha@ignitionadvice.com>
2020-03-22 10:42:57 -0700

    Update controller to use view model and copy new photos to images.

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using EmployeeManagement.Models;
 using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers
@@ -11,10 +13,13 @@ namespace EmployeeManagement.Controllers
     public class HomeController : Controller
     {
         private readonly IEmployeeRepository employeeRepository;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public HomeController(IEmployeeRepository employeeRepository)
+        public HomeController(IEmployeeRepository employeeRepository,
+            IWebHostEnvironment webHostEnvironment)
         {
             this.employeeRepository = employeeRepository;
+            this.webHostEnvironment = webHostEnvironment;
         }
     
         public ViewResult Index()
@@ -41,11 +46,33 @@ namespace EmployeeManagement.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Employee employee)
+        public IActionResult Create(EmployeeCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var newEmployee = employeeRepository.AddEmployee(employee);
+                string fileName = null;
+                if (model.Photo != null)
+                {
+                    var uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                    fileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    var filePath = Path.Combine(uploadsFolder, fileName);
+
+                    var fileStream = new FileStream(filePath, FileMode.Create);
+
+                    model.Photo.CopyTo(fileStream);
+
+                }
+
+                var newEmployee = new Employee
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    PhotoPath = fileName
+                };
+
+
+                employeeRepository.AddEmployee(newEmployee);
                 return RedirectToAction("details", new { id = newEmployee.Id });
             }
 
