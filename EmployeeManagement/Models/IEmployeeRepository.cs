843159e7bda1e74812ac9f3b525664d15b974904
Michael Ha <michael.ha@ignitionadvice.com>
2020-03-21 22:58:14 -0700

    Add ability to create new employee and redirect controller create action to employee details page.

--- EmployeeManagement/Models/IEmployeeRepository.cs
+++ EmployeeManagement/Models/IEmployeeRepository.cs
@@ -9,5 +9,6 @@ namespace EmployeeManagement.Models
     {
         Employee GetEmployee(int id);
         IEnumerable<Employee> GetAllEmployees();
+        Employee AddEmployee(Employee employee);
     }
 }
