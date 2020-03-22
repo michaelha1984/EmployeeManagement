d4b915921444ee29f19b0d4005d23746dc642a40
Michael Ha <michael.ha@ignitionadvice.com>
2020-03-22 09:33:31 -0700

    Add model builder extensions and move seed data into it.

--- EmployeeManagement/Models/AppDbContext.cs
+++ EmployeeManagement/Models/AppDbContext.cs
@@ -17,29 +17,7 @@ namespace EmployeeManagement.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Employee>().HasData(
-                new Employee
-                {
-                    Id = 1,
-                    Name = "Michael",
-                    Department = Dept.IT,
-                    Email = "michael@company.com"
-                },
-                new Employee
-                {
-                    Id = 2,
-                    Name = "Mick",
-                    Department = Dept.Payroll,
-                    Email = "mick@company.com"
-                },
-                new Employee
-                {
-                    Id = 3,
-                    Name = "Michelle",
-                    Department = Dept.HR,
-                    Email = "michelle@company.com"
-                }
-                );
+            modelBuilder.Seed();
         }
     }
 }
