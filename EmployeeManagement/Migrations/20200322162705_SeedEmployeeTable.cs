d84ea500985f5a99bfa0d62044369f42c96bdc78
Michael Ha <michael.ha@ignitionadvice.com>
2020-03-22 09:27:37 -0700

    Add seed data to employee table using ef migration.

---
+++ EmployeeManagement/Migrations/20200322162705_SeedEmployeeTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class SeedEmployeeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 1, 2, "michael@company.com", "Michael" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 3, "mick@company.com", "Mick" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 3, 1, "michelle@company.com", "Michelle" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
